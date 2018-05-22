86abe67ded25a6625163b6062c7da311c9941636
to003 <Okomba22>
2018-05-22 10:24:50 -0400

    test

--- app/src/main/java/com/example/to003/metzapp/MainActivity.java
+++ app/src/main/java/com/example/to003/metzapp/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.to003.metzapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends android.app.Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(android.view.Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
     }
 
 }
